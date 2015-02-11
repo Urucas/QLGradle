5a71569a3861a0dc1b454bd98ae87bad6ac7a687
vrunoa <vruno.alassia@gmail.com>
2015-02-10 23:17:21 -0300

    first view of .gradle preview

--- QLGradle/GeneratePreviewForURL.m
+++ QLGradle/GeneratePreviewForURL.m
@@ -22,58 +22,16 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
     @autoreleasepool {
         
         NSURL *URL = (__bridge NSURL *)url;
-        NSLog(@"url %@", url);
-        
         NSString *dataType = (__bridge NSString *)contentTypeUTI;
-        NSLog(@"type %@", dataType);
         
-        NSMutableDictionary *maxSize = [NSMutableDictionary alloc];
-
-        float width = 1024.0;
-        float height = 800.0;
-        
-        NSData* data = [NSData dataWithContentsOfURL:URL];
-        
-        if (data) {
-            NSRect viewRect = NSMakeRect(0.0, 0.0, 600.0, 800.0);
-            float scale = height / 800.0;
-            NSSize scaleSize = NSMakeSize(scale, scale);
-            CGSize thumbSize = NSSizeToCGSize(
-                                              NSMakeSize((width * (600.0/800.0)),
-                                                         height));
+        if([dataType isEqualToString:@"dyn.ah62d4rv4ge80s6xbqv0gn"]) {
             
-            WebView* webView = [[WebView alloc] initWithFrame: viewRect];
-            [webView scaleUnitSquareToSize: scaleSize];
-            [[[webView mainFrame] frameView] setAllowsScrolling:NO];
-            [[webView mainFrame] loadData: data
-                                 MIMEType: @"text/html"
-                         textEncodingName: @"utf-8"
-                                  baseURL: nil];
-            
-            while([webView isLoading]) {
-                CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true);
-            }
-            
-            [webView display];
-            
-            //CGContextRef context =
-            //QLThumbnailRequestCreateContext(thumbnail, thumbSize, false, NULL);
-            
-            /*
-            if (context) {
-                NSGraphicsContext* nsContext =
-                [NSGraphicsContext
-                 graphicsContextWithGraphicsPort: (void*) context
-                 flipped: [webView isFlipped]];
-                
-                [webView displayRectIgnoringOpacity: [webView bounds]
-                                          inContext: nsContext];
-                
-                QLThumbnailRequestFlushContext(thumbnail, context);
-                
-                CFRelease(context);
-            }
-             */
+            QLPreviewRequestSetURLRepresentation(
+                preview,
+                url,
+                kUTTypePlainText,
+                NULL
+            );
         }
         
     }
